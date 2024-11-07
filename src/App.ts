913ec09725e5bc8cd9523234e43aa6808378773b
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-11-07 19:31:57 -0300

    up

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 1rem;
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+  padding-top: 2rem;
+  max-width: 46rem;
+  margin: 0 auto;
+
+  .imgFundo {
+    position: absolute;
+    z-index: -1;
+    left: -3px;
+    bottom: -4rem;
+
+    background-image: url("./assets/fvgg.png");
+    background-repeat: no-repeat;
+    height: 50%;
+    width: 100%;
+  }
 `;
 
 export const Header = styled.div`
@@ -9,32 +26,72 @@ export const Header = styled.div`
   justify-content: center;
   align-items: center;
 
+  background: rgb(131, 58, 180);
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(95, 62, 130, 1) 50%,
+    rgba(150, 47, 148, 1) 100%
+  );
+
   .svgLupa {
-    height: 5rem;
-    width: 5rem;
+    height: 5.5rem;
+    width: 5.5rem;
+  }
+
+  h1 {
+    line-height: 3rem;
   }
 `;
 
 export const Main = styled.div`
-padding-top: 1.5rem;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 5px;
-  .sites {
-    margin-bottom: 1rem;
-    button {
-      background: transparent;
-      border: none;
-    }
+  padding: 1rem;
+  padding-top: 1.5rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 5px;
+  grid-row-gap: 1rem;
 
-    border-radius: 2rem;
-    padding: 1.5rem;
+  .sites-wrapper {
+    background: linear-gradient(
+      90deg,
+      rgba(41, 60, 87, 1) 0%,
+      rgba(209, 119, 235, 1) 50%,
+      rgba(67, 45, 73, 1) 100%
+    );
+    padding: 5px; /* Define a espessura da "borda" */
+    border-radius: 12px; /* Define o arredondamento */
+    display: inline-block; /* Para que o wrapper ajuste ao conteúdo */
+  }
+
+  .sites,
+  .creator {
+    //margin-bottom: 1rem;
+    text-align: center;
+    border-radius: 1rem;
+    padding: 1rem;
     background: rgb(24, 30, 128);
     background: linear-gradient(
       180deg,
       rgba(24, 30, 128, 1) 0%,
       rgba(201, 182, 235, 1) 100%
     );
+
+    button {
+      background: transparent;
+      border: none;
+
+      p {
+        font-size: 1.6rem;
+        font-weight: bold;
+      }
+    }
+  }
+
+  .creator {
+    grid-column: 1 / -1; /* Expande o item para ambas as colunas */
+    text-align: center;
+    justify-content: center;
+    margin: 0 auto;
   }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,106 @@
 import { Container, Header, Main } from "./App";
-import { BiSearchAlt } from "react-icons/bi";
-import imgFundo from './assets/DALL.webp'
+import { CiSearch } from "react-icons/ci";
+import imgFundo from "./assets/hhh.png";
 
 function App() {
   return (
     <Container>
+      <img className="imgFundo" src={imgFundo} />
       <Header>
-        <BiSearchAlt className="svgLupa"/>
-
-        <h1>Explorando <br/>a Gamificação</h1>
+        <h1>
+          Explorando <br />a Gamificação
+        </h1>
+        <CiSearch className="svgLupa" />
       </Header>
 
       <Main>
-        <div className="sites">
-          <button>
-          <p>Artes & Humanas</p>
-          </button>
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p> Humanidades</p>
+            </button>
+          </div>
         </div>
 
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Tecnologia </p>
+            </button>
+          </div>
         </div>
 
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Ciências</p>
+            </button>
+          </div>
         </div>
 
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
-        </div>
-        <div className="sites">
-        <button>
-          <p>Artes & Humanas</p>
-          </button>
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Matemática</p>
+            </button>
+          </div>
         </div>
 
-        
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Idiomas</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Gestão</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Jogos</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Avalição</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Inovações</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="sites">
+            <button>
+              <p>Cursos</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="sites-wrapper">
+          <div className="creator">
+            <button>
+              <p>Creator</p>
+            </button>
+          </div>
+        </div>
       </Main>
     </Container>
   );
